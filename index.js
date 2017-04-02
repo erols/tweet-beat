ea3420c2e39403e24ef5a0ad4e37d7eec8923b06
Erol Suleyman <erol.suleyman@gmail.com>
2017-04-02 20:15:04 +0200

    fix index.js

--- index.js
+++ index.js
@@ -1,25 +1,18 @@
 const express = require('express');
 const http = require('http');
 const url = require('url');
-<<<<<<< HEAD
+
 const WebSocket = require('ws');
 const Twitter = require('twitter');
 
 // replace with valid keys. I will supply on the day. These are not valid anymore
-var Twitter = require('twitter');
-
 var client = new Twitter({
->>>>>>> Changes to webserver
   consumer_key: 'himWvvF8H5Cj9igkvsRoZKqil',
   consumer_secret: '4eubxtfYtLzpTRSCUzxIiyF96bjb3wTxJrjCMR6n8a4ssZ51J7',
   access_token_key: '847758088610484224-EbwFm1ndzkKWFUNkSNgOwQDKrETT09D',
   access_token_secret: 'uP7cIiSfab38V7HwHEbSm17mcKDODlbHuvL4SyrL68lDU'
 });
  
-const params = {track: '@tweet_beat99'};
-
-console.log('after new Twitter');
-
 var params = {track: '@tweet_beat99'};
 const app = express();
 
